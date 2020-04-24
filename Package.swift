adb2d7c70732badaed894351bc9b9d3e6ce337bb
Dexter Liu <dexterliu1214@gmail.com>
2020-04-24 11:33:49 +0800

    fix dependency

--- Package.swift
+++ Package.swift
@@ -10,11 +10,14 @@ let package = Package(
         .library(
             name: "RxBinding",
             targets: ["RxBinding"]),
+        .library(
+     	    name: "RxCocoaRuntime",
+	    targets: ["RxBinding"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from:"5.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
