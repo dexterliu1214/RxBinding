6cf11ed6fb0d03d697af1ed67ec7a493cea93e4f
youga <dexterliu1214@gmail.com>
2020-04-24 11:21:07 +0800

    fix typo

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxBinding",
-            dependencies: ["RxSwift"]),
+            dependencies: [
+		"RxSwift",
+		"RxCocoa",
+	    ]),
         .testTarget(
             name: "RxBindingTests",
             dependencies: ["RxBinding"]),
