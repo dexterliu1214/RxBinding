e5a0adfcbe4102b9f0bfecb749bee38409502101
youga <dexterliu1214@gmail.com>
2020-04-24 13:33:06 +0800

    fix typo

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "RxBinding",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "RxBinding",
             targets: ["RxBinding"]),
@@ -15,13 +14,9 @@ let package = Package(
 	    targets: ["RxBinding"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxBinding",
             dependencies: [
